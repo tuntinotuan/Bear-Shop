c24cff94b760e8ae1f7f6d6640f46b6744dcdd1e
tuntinotuan <tuntino666@gmail.com>
2024-05-06 13:42:26 +0700

    Update header

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -4,11 +4,24 @@ import Button from "../button/Button";
 import HeaderLogo from "../logo/header.logo";
 import { Avatar } from "@nextui-org/react";
 import Hotline from "../contact/Hotline";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 
 const AppHeader = () => {
   return (
-    <section className="flex items-center justify-between px-5 py-2">
-      <HeaderLogo />
+    <section className="flex items-center justify-between px-5 py-2 max-sm:flex-col">
+      <div className="flex items-center gap-4">
+        <HeaderLogo />
+        <div className="flex items-center gap-2 font-bold max-md:flex-col">
+          <div className="flex items-center gap-2">
+            <p>Pass Gấu bông</p>
+            <FavoriteOutlinedIcon
+              color="error"
+              fontSize="large"
+            ></FavoriteOutlinedIcon>
+          </div>
+          Khu vực Tân Phú - HCM
+        </div>
+      </div>
       <a
         href="https://zalo.me/0814411007"
         target="_blank"
